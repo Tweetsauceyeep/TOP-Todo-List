b82f755e2c166bec6e4de659e82e9d331166a61b
Tweetsauce <pobusauce12@gmail.com>
2022-01-04 16:38:50 +0400

    up to date

--- src/modules/loadUI.js
+++ src/modules/loadUI.js
@@ -1,16 +1,14 @@
 import deleteTodo from './deleteTodo.js';
 import displayTodo from './displayTodo.js';
 import handleFormInput from './handleForm.js';
-//import addButtonListener from './modules/addButtonListener';
-//^ this func dont work bruh
 
 let content = document.querySelector('#content');
 let form = document.getElementById('inputForm');
 let submitButton = document.querySelector('#formsubmit');
 let deleteButton = document.getElementsByClassName('deletebutton');
 let defaultProjButton = document.querySelector('#defaultproject');
+
 // press button to add todo after filling out form
-//
 function loadUI() {
   submitButton.addEventListener('click', () => {
     content.appendChild(displayTodo(handleFormInput(form)));
@@ -19,6 +17,7 @@ function loadUI() {
     deleteTodo(deleteButton);
   });
 
+  // experiment: make div invisible on button press.
   defaultProjButton.addEventListener('click', () => {
     if (content.className === 'invisibleDiv') {
       content.style.display = 'block';
@@ -27,4 +26,4 @@ function loadUI() {
     }
   });
 }
-export default loadUI
+export default loadUI;
